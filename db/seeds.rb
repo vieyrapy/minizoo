08867ce8dfb94578b7c9ea9acbf6f5f585257c0f
vieyrapy <latinotanato@gmail.com>
2017-09-06 23:26:18 -0400

    Configurar una carga por defecto para un usuario

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#-----------------------------------------------------------
+# Configurar una carga por defecto para nuestro usuario
+#Usuario.create(email: "adminzoo@minizoo.com", password: "minizoo2018", password_confirmation: "minizoo2018")
