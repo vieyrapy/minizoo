2c08fb751e214d2f60e4cfea34530f74d55c61ad
vieyrapy <latinotanato@gmail.com>
2017-09-10 21:26:13 -0400

    Articulo para las noticias

---
+++ test/system/articulos_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class ArticulosTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit articulos_url
+  #
+  #   assert_selector "h1", text: "Articulo"
+  # end
+end
