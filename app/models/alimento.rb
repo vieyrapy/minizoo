eec0415005d1f9b4b268195fb6fe88e6cefc8a8e
vieyrapy <latinotanato@gmail.com>
2017-09-17 13:47:59 -0400

    Validate Alimento

--- app/models/alimento.rb
+++ app/models/alimento.rb
@@ -1,2 +1,4 @@
 class Alimento < ApplicationRecord
+validates :nombre,  presence: true
+validates :nombre, length: { minimum: 4, maximum: 10}, format: { with: /\A[a-zA-Z]+\z/}#Format es donde configuramos el formato aceptado en el campo en este caso solo aceptamos letras
 end
