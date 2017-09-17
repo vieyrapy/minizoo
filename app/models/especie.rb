8cb41a0ab1668a6a8bf22efb0f8f238c2e610120
vieyrapy <latinotanato@gmail.com>
2017-09-16 20:40:28 -0400

    Validaciones para especie

--- app/models/especie.rb
+++ app/models/especie.rb
@@ -1,2 +1,4 @@
 class Especie < ApplicationRecord
+	validates :nombre,  presence: true
+	validates :nombre, length: { minimum: 4, maximum: 10}, format: { with: /\A[a-zA-Z]+\z/}#Format es donde configuramos el formato aceptado en el campo en este caso solo aceptamos letras
 end
