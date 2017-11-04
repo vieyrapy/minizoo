308a9c91acd955c17df4bb3eb8e31ac0dca86e7d
vieyrapy <latinotanato@gmail.com>
2017-11-04 18:47:45 -0300

    Cambiar validación

--- app/models/cite.rb
+++ app/models/cite.rb
@@ -1,5 +1,5 @@
 class Cite < ApplicationRecord
 	validates :nombre,  presence: true
-	validates :nombre, length: { minimum: 2, maximum: 5}
+	validates :nombre, length: { minimum: 1, maximum: 8}
 	has_many :animales
 end
