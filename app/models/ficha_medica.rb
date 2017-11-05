065fc4e595cae26f399dc9ef7dbcf3b0b2c47f2e
vieyrapy <latinotanato@gmail.com>
2017-11-04 21:02:18 -0300

    Validaciones Ficha Medica

--- app/models/ficha_medica.rb
+++ app/models/ficha_medica.rb
@@ -3,6 +3,12 @@ class FichaMedica < ApplicationRecord
 	mount_uploader :archivos, AvatarUploader
 	mount_uploader :audio, AvatarUploader
 	mount_uploader :video, AvatarUploader
+
+	validates :dolencia, :presence => {message: "El Pre-diagnóstico no puede estar en blanco"} 
+	validates :veterinario, :presence => {message: "Por favor introduzca el nombre del Veterinario/Medico" }
+	validates :enfermedad, :presence => {message: "El Motivo de la consulta no puede estar en blanco" }
+	validates :descripcion, length: { maximum: 1000}
+	validates :archivos, presence: false
 	belongs_to :animal
 	belongs_to :estado_medico
 	
@@ -14,4 +20,3 @@ class FichaMedica < ApplicationRecord
     	enfermedad
   	end
 end
- 
