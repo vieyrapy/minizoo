57982464d0a50868a8ed034412a949509743b36d
vieyrapy <latinotanato@gmail.com>
2018-02-07 21:45:56 -0300

    Corregir migraciones con tablas que fueron eliminado

--- db/migrate/20170922223655_agregar_clave_foranea_a_ficha_medica.rb
+++ db/migrate/20170922223655_agregar_clave_foranea_a_ficha_medica.rb
@@ -3,7 +3,7 @@ class AgregarClaveForaneaAFichaMedica < ActiveRecord::Migration[5.1]
   	add_column :ficha_medicas, :animal_id, :integer
     add_foreign_key(:ficha_medicas, :animales) 
 	# Agregamos columna en internados en la relacion ficha_medica con internados
-    add_column :internados, :ficha_medica_id, :integer
-    add_foreign_key(:internados, :ficha_medicas) 
+    #add_column :internados, :ficha_medica_id, :integer
+    #add_foreign_key(:internados, :ficha_medicas) 
   end
 end
